f1dd464b4f6ef49024c8ac6ab5d0865568f09f0f
mohamuhsin <muhsinohoho@outlook.com>
2025-02-25 18:43:20 +0300

    added floating whatsapp

---
+++ components/WhatsApp/Whatsapp.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Whatsapp() {
+    return (
+        <Link
+            href="https://wa.me/256782330038"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Chat with us on WhatsApp"
+            className="whatsapp-button fixed bottom-6 right-6 z-50 rounded-full shadow-lg p-4 transition-all"
+        >
+            <Image
+                src="/whatsapp.png"
+                alt="WhatsApp"
+                width={80}
+                height={80}
+                className="rounded-full"
+            />
+        </Link>
+    );
+}
