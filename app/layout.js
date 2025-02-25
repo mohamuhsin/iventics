7321fc55a0d4ba5911ea870a66958ca90e9aab5e
mohamuhsin <muhsinohoho@outlook.com>
2025-02-25 19:01:26 +0300

    added font

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
-
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Whatsapp from "@/components/WhatsApp/Whatsapp";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const montserrat = Montserrat({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata = {
@@ -21,9 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${montserrat.variable} antialiased`}>
         {children}
         <Whatsapp />
       </body>
