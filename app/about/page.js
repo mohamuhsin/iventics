a021984859b9f69803ac172f10409e3600f396f4
Moha Muhsin <mohamuhsin101@gmail.com>
2025-06-15 02:04:44 +0300

    page header update

--- app/about/page.js
+++ app/about/page.js
@@ -5,7 +5,7 @@ export default function AboutPage() {
         <>
             <PageHeader
                 title="About Us"
-                subtitle="Get to know our mission, values, and the team powering our digital solutions."
+                subtitle="Get to know our mission, values, and the team powering our digital solutions and delivering trusted ICT services."
             />
             {/* Rest of your About Us page content */}
         </>
